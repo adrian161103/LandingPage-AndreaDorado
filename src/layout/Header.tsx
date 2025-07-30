1cebde412100ccc41678fb3b2a60b88ada7ca7ee
adrian161103 <adriankeomaa@gmail.com>
2025-07-30 01:05:30 -0300

    header colores mismos que el footer para mantener la estetica

--- src/layout/Header.tsx
+++ src/layout/Header.tsx
@@ -10,13 +10,13 @@ interface HeaderProps {
 }
 
 const Header: React.FC<HeaderProps> = ({ navLinks }) => (
-  <header className="w-full bg-white shadow-md sticky top-0 z-50">
+  <header className="w-full bg-[#F8F4ED] text-[#7F8C9A] shadow-md sticky top-0 z-50">
     <nav className="container mx-auto flex items-center justify-between py-4 px-6">
-      <div className="text-xl font-bold">Andrea Dorado</div>
+      <div className="text-xl font-bold"> <a href="">Andrea Dorado</a></div>
       <ul className="flex space-x-6">
         {navLinks.map((link) => (
           <li key={link.href}>
-            <a href={link.href} className="hover:text-blue-600 transition-colors">
+            <a href={link.href} className="hover:text-[#7BBBCD] transition-colors duration-[500ms]">
               {link.label}
             </a>
           </li>
