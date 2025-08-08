3b46f6c7ff2286ddc8809ad5deca796bbae2d577
adrian161103 <adriankeomaa@gmail.com>
2025-08-08 02:46:41 -0300

    helmet sacado, error de compatibilidad

--- src/components/Page.tsx
+++ src/components/Page.tsx
@@ -1,15 +1,12 @@
 import React from "react";
-import { FaFacebookF, FaInstagram, FaWhatsapp  } from "react-icons/fa";
-import { SiGooglemaps, SiMercadopago, SiVisa, SiMastercard  } from "react-icons/si";
-import { TbCashBanknote  } from "react-icons/tb";
-
+import { FaFacebookF, FaInstagram, FaWhatsapp } from "react-icons/fa";
+import { SiGooglemaps, SiMercadopago, SiVisa, SiMastercard } from "react-icons/si";
+import { TbCashBanknote } from "react-icons/tb";
 
 import Footer from "../layout/Footer";
 import Header from "../layout/Header";
 import type { Social, NavLink, ContactInfo } from "../layout/Footer";
 import type { HeaderNavLink } from "../layout/Header";
-
-import { Helmet } from "react-helmet";
 
 const navLinks: NavLink[] = [
   { label: "Inicio", href: "#hero" },
@@ -27,22 +24,18 @@ const socials: Social[] = [
   { name: "WhatsApp", url: "https://wa.me/5491151091962", icon: <FaWhatsapp />, color: "#25D366" },
 ];
 
-
 const contactInfo: ContactInfo = {
   phone: "+54 9 1151091962",
   email: "lic.andreadorado@gmail.com",
   href: "https://maps.app.goo.gl/kju5aMR1CBLK1EmA7",
-  addresses: [
-    "Jerónimo Salguero 1164, Almagro | Palermo",
-    "CABA",
-  ],
+  addresses: ["Jerónimo Salguero 1164, Almagro | Palermo", "CABA"],
 };
 
 const paymentMethods = [
   { icon: <SiMercadopago />, label: "Mercado Pago", color: "#009EE3" },
   { icon: <SiVisa />, label: "Visa", color: "#1A1F71" },
   { icon: <SiMastercard />, label: "Mastercard", color: "#EB001B" },
-  { icon: <TbCashBanknote  />, label: "Efectivo", color: "#08b651" },
+  { icon: <TbCashBanknote />, label: "Efectivo", color: "#08b651" },
 ];
 
 interface PageProps {
@@ -51,16 +44,19 @@ interface PageProps {
   children: React.ReactNode;
 }
 
-const Page: React.FC<PageProps> = ({ title = "Andrea Dorado | Psicoanalista", description = "Lic. Andrea Dorado - Atención Psicoanalítica, Trayectoria, El Andén, Contacto.", children }) => {
+const Page: React.FC<PageProps> = ({
+  title = "Andrea Dorado | Psicoanalista",
+  description = "Lic. Andrea Dorado - Atención Psicoanalítica, Trayectoria, El Andén, Contacto.",
+  children,
+}) => {
   return (
     <>
-      <Helmet>
-        <title>{title}</title>
-        <meta name="description" content={description} />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <meta charSet="utf-8" />
-        <link rel="canonical" href="https://andreadorado.com.ar/" />
-      </Helmet>
+      <title>{title}</title>
+      <meta name="description" content={description} />
+      <meta name="viewport" content="width=device-width, initial-scale=1" />
+      <meta charSet="utf-8" />
+      <link rel="canonical" href="https://andreadorado.com.ar/" />
+
       <div className="flex flex-col min-h-screen">
         <Header navLinks={headerLinks} />
         <main className="flex-1">{children}</main>
